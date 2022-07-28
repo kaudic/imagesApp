9346ea4fe9d044e7b1896c0bd4e137c48057af7e
Killian Audic <killianaudic@aol.fr>
2022-07-28 12:59:47 +0200

    tracking proxy request

--- app/index.js
+++ app/index.js
@@ -12,6 +12,23 @@ app.use(express.static('./public'));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+
+    // if the full url is http://audicserver.ddns.net:3000/toto
+    // "/toto" can be found in req.path or req.originalUrl
+
+    console.log(`req.path: ${req.path}`);
+    console.log(`req.subdomains: ${req.subdomains}`);
+    console.log(`req.baseUrl: ${req.baseUrl}`);
+    console.log(`req.hostname: ${req.hostname}`);
+    console.log(`req.originalUrl: ${req.originalUrl}`);
+    console.log(`req.path: ${req.path}`);
+
+    next();
+
+});
+
 app.use(router);
 
 module.exports = app;
